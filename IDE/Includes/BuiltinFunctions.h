23e1666b6d3c82c54528964415c109c95f0a8cef
Odie Edo-Osagie <O.Edo-Osagie@uea.ac.uk>
2015-11-15 02:04:49 +0000

    Added Gary IDE

---
+++ IDE/Includes/BuiltinFunctions.h
@@ -0,0 +1,24 @@
+
+#include <iostream>
+#include <vector>
+
+template<typename T> void printVector(std::vector<T> v)
+{
+    for (int i = 0; i < v.size(); i++){
+        if(i == 0){
+            /* First element */
+            std::cout << "[" << v[i] << ", ";
+        }
+        else if(i == v.size()-1){
+            /* Last element */
+            std::cout << v[i] << "]" << std::endl;
+        }
+        else{
+            /* Other elements */
+            std::cout << v[i] << ", ";
+        }
+        
+    }
+}
+
+
